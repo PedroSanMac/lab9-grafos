7a0e85a647bffb1a21c98cd1432c78c430dfa61c
Pedr0 <psanchezm@unsa.edu.pe>
2021-08-07 09:28:13 -0500

    Implementado grafo no dirigido con lista Adyacencia

--- src/GrafoAdyacencia.java
+++ src/GrafoAdyacencia.java
@@ -1,8 +1,8 @@
 
 
-public class GrafoAdyacencia {
+public class GrafoAdyacencia<T> {
 	
-	private class Vertice<E>{
+	public class Vertice<E>{//clase vertice
         public E data;
         public ListaEnlazada<Arista<E>> listAdj;
         public Vertice(E val){
@@ -37,6 +37,37 @@ public class GrafoAdyacencia {
 		}
 	}
 	
+	ListaEnlazada<Vertice<T>> listaVertices;
 	
+	public GrafoAdyacencia() {
+		listaVertices = new ListaEnlazada<Vertice<T>>();
+	}
+	public void insertVertice(T data) {
+		Vertice<T> nuevo = new Vertice<T>(data);
+		if(this.listaVertices.search(nuevo) != null) {
+			System.out.println("VERTICE YA INSERTADO..");
+			return;
+		}
+		this.listaVertices.insertPrimero(nuevo);
+	}
+	public void insertArista(T verOri, T verDes) {
+		Vertice<T> refOri =this.listaVertices.search(new Vertice<T>(verOri));
+		Vertice<T> refDes =this.listaVertices.search(new Vertice<T>(verDes));
+		
+		if(refOri == null || refDes ==null) {
+			System.out.println("VERTIVE NO EXISTE...");
+			return;
+		}
+		if(refOri.listAdj.search(new Arista<T>(refDes)) != null) {
+			System.out.println("Arista ya insertada antes ...");
+			return;
+		}
+		//no dirigido.
+		refOri.listAdj.insertPrimero(new Arista<>(refDes));
+		refDes.listAdj.insertPrimero(new Arista<T>(refOri));
+	}
+	public String toString() {
+		return this.listaVertices.toString();
+	}
 
 }
